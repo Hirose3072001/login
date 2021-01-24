c0ac62f925cd2d0177a7cab5587740ee880d441c
Trinh_Cong_Minh <congminhtrinh88@gmail.com>
2021-01-24 08:07:44 +0700

    aaaa
    aaaa

---
+++ trac_nghiem_online_by_Minh/static/js/login.js
@@ -0,0 +1,39 @@
+const togglePassword=document.getElementById("togglePassword");
+const inputPassword=document.getElementById("password");
+togglePassword.addEventListener("click",function()
+{
+    togglePassword.classList.toggle("fa-eye-slash");
+    if (inputPassword.type === "password") {
+        inputPassword.type = "text";
+      } else {
+        inputPassword.type = "password";
+      }
+   
+});
+
+function validateForm()
+{
+   
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+ 
+    
+    if (username == ''){
+        alert('Bạn chưa nhập tên đăng nhập');
+    }
+    else if (password == '')
+    {
+        alert('Bạn chưa nhập mật khẩu !');
+    }
+    else{
+        if(username=="kitweb2001@gmail.com"&&password=="kitwebkma2001")
+        {
+            window.location.href = "index.html";
+        }
+        else{
+            alert('Tài khoản hoặc mật khẩu chưa đúng. Xin thử lại !');
+        }
+    }
+ 
+    return false;
+}
